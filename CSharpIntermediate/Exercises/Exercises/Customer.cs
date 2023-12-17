7f5d73e9359eb28945d854a280cd7a6dc0c8462d
DESKTOP-399M2SR\Admin <adityapandey0264@gmail.com>
2023-12-17 10:28:09 +0530

    Constructors.

---
+++ CSharpIntermediate/Exercises/Exercises/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders; //A list is a generic class and its uses a parameter called generic parameter.
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+        public Customer(int id)
+            :this()
+        {
+            this.Id = id;
+        }
+        public Customer(int id,string name)
+            :this(id)
+        {
+            
+            this.Name = name;
+        }
+    }
+}
---
+++ CSharpIntermediate/Exercises/Exercises/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1,"john");
+            //Console.WriteLine(customer.Id);
+            //Console.WriteLine(customer.Name);
+            Console.ReadKey();
+
+        }
+    }
+}
